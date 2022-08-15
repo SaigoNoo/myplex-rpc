66209f0f6bdd8c5a3ba605778b7e98dad3633b57
SaigoNoo <40198990+SaigoNoo@users.noreply.github.com>
2022-08-15 23:18:12 +0200

    Add files via upload

---
+++ run.py
@@ -0,0 +1,110 @@
+import json
+import time
+from plexapi.server import PlexServer
+from pypresence import Presence
+
+# Variables / Array
+plexArray = []
+waitTime = 0.5
+connect = False
+change = False
+last = ""
+start = True
+
+# Config Load
+with open("config.json") as config:
+    data = json.load(config)
+
+    https = data["plex"]["https"]
+    url = data["plex"]["url"]
+    port = data["plex"]["port"]
+    token = data["plex"]["token"]
+    user = data["plex"]["user"]
+
+full_url = f"{'https' if https else 'http'}://{url}:{port}"
+plex = PlexServer(full_url, token)
+
+# Wait a activity to start !
+while len(plex.sessions()) == 0:
+    print(f"En l'attente d'une activité du coté serveur... {waitTime} secondes...")
+    time.sleep(waitTime)
+
+
+# Functions
+def update_metas(array, session_var, token_var):
+    array.clear()
+
+    if session_var.type == "track":
+        array += [session_var.title]
+        array += [session_var.artist().title]
+        array += [f"{full_url}{session_var.thumb}?X-Plex-Token={token_var}"]
+    elif session_var.type == "episode":
+        array += [session_var.show().title]
+        array += [session_var.title]
+        array += [f"{full_url}{session_var.show().thumb}?X-Plex-Token={token_var}"]
+    elif session_var.type == "movie":
+        array += [session_var.title]
+        genres = ""
+        for genre in session_var.genres:
+            genres += f"{str(genre).split(':')[2].replace('>', '')} "
+        array += [genres.replace(" ", ", ")[:-1]]
+        array += [f"{full_url}{session_var.thumb}?X-Plex-Token={token_var}"]
+        array += [session_var.duration]
+
+
+def update_presence(array, session_var):
+    print(session_var.duration, session_var.viewOffset)
+    rpc.update(
+        details=str(array[0]),
+        state=str(array[1]),
+        large_image=str(array[2]),
+        large_text=str(array[0]),
+        end=int(time.time() + (session_var.duration / 1000) - (session_var.viewOffset / 1000))
+    )
+
+
+def as_change(last_var, actual):
+    if str(last_var) != str(actual.ratingKey):
+        return True
+    else:
+        return False
+
+
+# Presence Loop
+rpc = Presence(data["discord_id"])
+
+while True:
+    if len(plex.sessions()) == 0:
+        connect = False
+        rpc.close()
+
+    while len(plex.sessions()) == 0:
+        time.sleep(waitTime)
+        print("En attente d'activité !")
+
+    for [index, session] in enumerate(plex.sessions()):
+        if user in session.usernames:
+            time.sleep(waitTime)
+            if start:
+                start = False
+                last = str(session.ratingKey)
+
+            print(plexArray)
+            update_metas(plexArray, session, token)
+
+            if not connect:
+                connect = True
+                rpc.connect()
+                update_presence(plexArray, session)
+
+            if session.players[0].state == "paused":
+                update_presence(plexArray, session)
+
+            while len(plex.sessions()) == 0:
+                time.sleep(waitTime)
+                print("En attente d'activité !")
+
+            if as_change(last, session):
+                last = session.ratingKey
+                update_metas(plexArray, session, token)
+                update_presence(plexArray, session)
